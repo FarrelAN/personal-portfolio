9c5dd8c3869530150dd07980f9f5d274d525e793
FarrelAN <m.farrel@ui.ac.id>
2023-11-27 04:56:04 +0700

    major fixes 1.2

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -29,9 +29,7 @@ const Navbar = () => {
     <nav className="fixed top-0 left-0 right-0 z-50 bg-[#121212] bg-opacity-100">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
-          to="home"
-          smooth={true}
-          duration={500}
+          href={"/"}
           className="text-2xl md:text-5xl text-white font-semibold"
         >
           FarrelAN
@@ -56,10 +54,8 @@ const Navbar = () => {
         <div className="menu hidden md:block md:w-auto" id="navbar">
           <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
             {navLinks.map((link, index) => (
-              <li className="text-lg">
-                <Link to="home" smooth={true} duration={500}>
-                  Home
-                </Link>
+              <li key={index}>
+                <NavLink href={link.path} title={link.title} />
               </li>
             ))}
           </ul>
